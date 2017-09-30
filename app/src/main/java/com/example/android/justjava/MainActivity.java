0514d876974c9304e0e352c9e051b09c09782ef4
Carlos Ruiz <carlosruiz314@gmail.com>
2017-10-01 00:44:54 +0200

    Added mail intent to main java activity

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,6 +5,8 @@
 package com.example.android.justjava;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -83,7 +85,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Place order
         String orderSummary = createOrderSummary(userName, price, hasWhippedCream, hasChocolate);
-        displayMessage(orderSummary);
+//        displayMessage(orderSummary);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
